c30902ed8324d26d3b6a1108aedb0194474c77de
Alex <jlex.wd@outlook.com>
2017-07-21 22:24:20 -0600

    arg name to templateurl in template/component

--- templates/component.js
+++ templates/component.js
@@ -5,7 +5,7 @@ import ${input}Ctrl from './${input}.ctrl.js';
 export const ${input}Component = {
     controller: ${input}Ctrl,
     controllerAs: '${input}',
-    template: './partials/${input}.component.html'
+    templateUrl: './partials/${input}/${input}.component.html'
 };
 
 `
